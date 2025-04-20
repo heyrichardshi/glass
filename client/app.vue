6c6cfc45d09ab01abd02453eb6286404b1b2e589
Richard Shi <4483400+heyrichardshi@users.noreply.github.com>
2025-04-20 00:40:35 -0400

    feat(client): Added a navigation bar to all pages with links to home (transactions) page and accounts page.

--- client/app.vue
+++ client/app.vue
@@ -1,5 +1,6 @@
 <template>
   <UApp>
+    <Navigation />
     <NuxtPage />
   </UApp>
 </template>
---
+++ client/components/Navigation.vue
@@ -0,0 +1,20 @@
+<template>
+  <UNavigationMenu :items="items" class="w-full justify-center" />
+</template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const items = ref<NavigationMenuItem[]>([
+  {
+    label: "Transactions",
+    icon: "i-lucide-credit-card",
+    to: "/",
+  },
+  {
+    label: "Accounts",
+    icon: "i-lucide-landmark",
+    to: "/accounts",
+  },
+]);
+</script>
